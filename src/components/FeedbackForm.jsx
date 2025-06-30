9399d32a822eb2c244cdfaf8c0bf95232c50f5f4
Backa228 <andrey.scott01@gmail.com>
2025-06-30 12:02:38 +0300

    Update FeedbackForm.jsx
    в бібліотеці Formic розібрали компонент ErrorMessage

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,23 +1,28 @@
-import { Formik, Form, Field } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import css from './FeedbackForm.module.css'
 import { useId } from 'react'
 import * as Yup from 'yup'
 
 const FeedbackSchema = Yup.object().shape({
   username: Yup.string().min(2, 'Too Short!').max(50, "TooLong!").required("Required")
+  email: Yup.string().email("Must be a valid email!").required("Required"),
+  message: Yup.string().min(3, "Too short").max(256, "Too long").required("Required"),
+  selector: Yup.string().oneOf(["o1", "o2", "o3"]).required("Required")
 })
 
 const FeedbackForm = () => {
   const initialValue = {
-    username: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
-    email: Yup.string().email("Must be a valid email!").required("Required"),
-    message: Yup.string().min(3, "Too short").max(256, "Too long").required("Required"),
-    level: Yup.string().oneOf(["good", "neutral", "bad"]).required("Required")
+    username: "", 
+    email: "",
+    message: "",
+    selector: ""
 
   }
-  const handleSubmit = (values, actions) => {
-    console.log(values)
-    actions.resetForm()
+  const FeedbackForm = () => {
+    const handleSubmit = (values, actions) => {
+      console.log(values)
+      actions.resetForm()
+  }
   }
 
   const nameFieldId = useId()
@@ -30,6 +35,7 @@ const FeedbackForm = () => {
         <Form className={css.form}>
           <label htmlFor={emailFieldId}>UserName: </label>
           <Field id={nameFieldId} type="text" name="username" className={css.field1}></Field>
+          <ErrorMessage name="username" component="span" className={css.error}></ErrorMessage>
 
           <label htmlFor={emailFieldId}>Email:</label>
           <Field id={emailFieldId} type="email" name="email" className={css.field2}></Field>
