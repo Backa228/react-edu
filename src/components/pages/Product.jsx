86ab0c659a1d33e383faf2f32f4bbc6c2058ec86
Backa228 <andrey.scott01@gmail.com>
2025-11-24 16:49:09 +0200

    versel
    versel.json

--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 const products = [
   { id: 1, name: "iPhone 15" },
   { id: 2, name: "Samsung S24" },
+  { id: 3, name: "Google Pixel 8" },
+  { id: 4, name: "OnePlus 12" },
 ];
 
 function Products () {
--- src/components/pages/ProductDetails.jsx
+++ src/components/pages/ProductDetails.jsx
@@ -8,6 +8,10 @@ export const ProductDetails = () => {
   return (
     <div>
       <h2>Product ID: {productId}</h2>
+      {productId === '1' && <p>Details about iPhone 15...</p>}
+      {productId === '2' && <p>Details about Samsung S24...</p>}
+      {productId === '3' && <p>Details about Google Pixel 8...</p>}
+      {productId === '4' && <p>Details about OnePlus 12...</p>}
       <p>Here will be product details...</p>
     </div>
   );
